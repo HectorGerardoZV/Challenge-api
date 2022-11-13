e6da91a3ab5f0e3a20b5600d466b45fb2c9afd1a
HectorGerardoZV <torrozapata@gmail.com>
2022-11-12 22:45:03 -0700

    User Schema was modified to encrypt the password before to be added in the db

--- src/schemas/Users.schema.js
+++ src/schemas/Users.schema.js
@@ -1,4 +1,5 @@
 const { Schema, Types } = require("mongoose");
+const { hashSync, compare } = require("bcrypt");
 
 const Users = new Schema({
     email: {
@@ -16,5 +17,18 @@ const Users = new Schema({
         required: true,
     },
 });
+Users.pre("save", async function (next) {
+    try {
+        if (!this.isModified("password")) return next();
+        const passwordEncrypted = await hashSync(this.password, 10);
+        this.password = passwordEncrypted;
+        next();
+    } catch (error) {
+        next();
+    }
+});
+Users.methods.comparePassword = async function (password) {
+    return await compare(password, this.password);
+};
 
 module.exports = Users;
