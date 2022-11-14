80f8680f0428cce68ad673debeb9956a62057d75
HectorGerardoZV <torrozapata@gmail.com>
2022-11-14 02:54:49 -0700

    index file in helpers a routes were modified to use new functions

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,14 @@
 const router = require("express").Router();
+//Heloers
+const { accessHelpers } = require("../helpers");
+const { validateURLAccess } = accessHelpers;
 
 //Routers
 const authRouter = require("./auth.routes");
 const rolesRouter = require("./roles.routes");
 const usersRouter = require("./users.routes");
 
+router.use(validateURLAccess);
 router.use(authRouter);
 router.use(rolesRouter);
 router.use(usersRouter);
