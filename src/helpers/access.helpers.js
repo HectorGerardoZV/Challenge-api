a40cc6d335d98777bd0eccbb325b5ddc655f423a
HectorGerardoZV <torrozapata@gmail.com>
2022-11-14 02:55:36 -0700

    user validators and helpers were created and integred in the user pipeLine

---
+++ src/helpers/access.helpers.js
@@ -0,0 +1,60 @@
+const jwt = require("jsonwebtoken");
+const { SECRET } = require("../config/globals");
+const { RolesSchema, UsersSchema } = require("../schemas");
+
+const whiteList = [
+    {
+        POST: "/auth",
+        GET: "/auth",
+    },
+];
+
+const validateURLAccess = (req, res, next) => {
+    const urlRequest = `/${req.url.split("/")[1]}`;
+    const validURL = whiteList.find((urlItem) => urlItem[req.method] === urlRequest);
+    if (!validURL) return validateTokenMiddleware(req, res, next);
+    next();
+};
+
+const validateTokenMiddleware = (req, res, next) => {
+    try {
+        let token = "";
+        const { authorization } = req.headers;
+        if (authorization) {
+            token = authorization.toString().split(" ")[1];
+            const user = jwt.verify(token, SECRET);
+            if (!user) return res.status(401).json({ access: false });
+            const { role, id } = user;
+            req.user = { role, id };
+            return validateTokenRole(req, res, next);
+        } else {
+            res.status(401).json({ access: false });
+        }
+    } catch (error) {
+        return res.status(500).json({
+            msg: error.message,
+            access: false,
+        });
+    }
+};
+
+const validateTokenRole = async (req, res, next) => {
+    try {
+        const { user } = req;
+        const existUser = await UsersSchema.findOne({ _id: user.id });
+        const existRole = await RolesSchema.findOne({ _id: user.role });
+        if (!existUser || !existRole) return res.status(401).json({ access: false });
+        if (existRole.name !== "Admin" && existRole.name !== "SuperAdmin")
+            return res.status(401).json({ access: false });
+        next();
+    } catch (error) {
+        return res.status(500).json({
+            msg: error.message,
+            access: false,
+        });
+    }
+};
+
+module.exports = {
+    validateURLAccess,
+};
