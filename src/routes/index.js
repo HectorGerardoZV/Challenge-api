a0e2386ad951d152355cbb875183296ede467cce
HectorGerardoZV <torrozapata@gmail.com>
2022-11-16 00:39:09 -0700

    Teamas routes were added

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,11 +8,13 @@ const authRouter = require("./auth.routes");
 const rolesRouter = require("./roles.routes");
 const usersRouter = require("./users.routes");
 const accountsRouter = require("./accounts.routes");
+const teamsRouter = require("./teams.routes");
 
 router.use(validateURLAccess);
 router.use(authRouter);
 router.use(rolesRouter);
 router.use(usersRouter);
 router.use(accountsRouter);
+router.use(teamsRouter);
 
 module.exports = router;
