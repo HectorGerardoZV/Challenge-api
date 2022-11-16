5d02c9e6ed868048cb523236df33d07da82a8118
HectorGerardoZV <torrozapata@gmail.com>
2022-11-16 00:38:53 -0700

    Teams pipeLines were created

--- src/pipeLines/index.js
+++ src/pipeLines/index.js
@@ -2,10 +2,12 @@ const pipeLinesAuth = require("./auth.pipes");
 const pipeLinesRoles = require("./roles.pipes");
 const pipeLinesUsers = require("./users.pipes");
 const pipeLinesAccounts = require("./accounts.pipes");
+const pipeLinesTeams = require("./teams.pipes");
 
 module.exports = {
     pipeLinesAuth,
     pipeLinesRoles,
     pipeLinesUsers,
     pipeLinesAccounts,
+    pipeLinesTeams,
 };
