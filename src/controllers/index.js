5211d48aed956c4b452f046ba536f3ced03ea859
HectorGerardoZV <torrozapata@gmail.com>
2022-11-16 00:36:36 -0700

    Teams controller was created

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -2,12 +2,12 @@ const authController = require("./auth.controllers");
 const rolesController = require("./roles.controllers");
 const usersController = require("./users.controller");
 const accountsController = require("./accounts.controllers");
-
-
+const teamsController = require("./teams.controllers");
 
 module.exports = {
     authController,
     usersController,
     rolesController,
-    accountsController
+    accountsController,
+    teamsController
 };
